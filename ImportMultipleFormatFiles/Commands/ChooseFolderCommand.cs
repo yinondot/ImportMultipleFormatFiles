b6d0249440d417b8a24cfdba50a975059dfd42e5
Yinnon Dotan <yinondot@gmail.com>
2018-11-02 16:08:50 +0200

    added bindings to open folder MVVM

---
+++ ImportMultipleFormatFiles/Commands/ChooseFolderCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using ImportMultipleFormatFiles.ViewModel;
+
+namespace ImportMultipleFormatFiles.Commands
+{
+   public class ChooseFolderCommand : ICommand
+   {
+      public MainViewModel Vm { get; set; }
+
+      public ChooseFolderCommand(MainViewModel vm)
+      {
+         Vm = vm;
+      }
+      public event EventHandler CanExecuteChanged;
+
+      public bool CanExecute(object parameter)
+      {
+         return true;
+      }
+
+      public void Execute(object parameter)
+      {
+         Vm.ChooseFolderMethod();
+
+      }
+   }
+}
--- ImportMultipleFormatFiles/ViewModel/MainViewModel.cs
+++ ImportMultipleFormatFiles/ViewModel/MainViewModel.cs
@@ -5,17 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using ImportMultipleFormatFiles.Models;
 using ImportMultipleFormatFiles.Converters;
+using ImportMultipleFormatFiles.Commands;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace ImportMultipleFormatFiles.ViewModel
 {
    public class MainViewModel
     {
-      ComboNameConverter comboNameConverter;
-      ComboTextWidthConverter comboTextWidthConverter;
+    public   ComboNameConverter comboNameConverter;
+     public ComboTextWidthConverter comboTextWidthConverter;
+
+      public ChooseFolderCommand ChooseFolderCommand { get; set; }
+
       public MainViewModel()
       {
          comboNameConverter = new ComboNameConverter();
          comboTextWidthConverter = new ComboTextWidthConverter();
+
+         ChooseFolderCommand = new ChooseFolderCommand(this);
+      }
+
+
+      public void ChooseFolderMethod()
+      {
+         CommonOpenFileDialog dlg = new CommonOpenFileDialog();
+         dlg.IsFolderPicker = true;
+         dlg.ShowDialog();
       }
     }
 }
